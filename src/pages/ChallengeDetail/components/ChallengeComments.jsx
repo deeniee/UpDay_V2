3f369be79822bf635bfe8915144a9439d9e335d2
deeniee <enbn2704@gmail.com>
2025-04-21 17:54:37 +0900

    fix: 배포 오류 해결 ChallengeComments.jsx

--- src/pages/ChallengeDetail/components/ChallengeComments.jsx
+++ src/pages/ChallengeDetail/components/ChallengeComments.jsx
@@ -34,7 +34,7 @@ const ChallengeComments = ({ postData }) => {
     useEffect(() => {
         const filtered = savedComments.filter((c) => c.postId === postData.id);
         setCommentList(filtered);
-    }, [postData.id]);
+    }, [postData.id, savedComments]);
 
     const updateAllCommentsStorage = (updatedCurrentComments) => {
         const otherComments = savedComments.filter(
